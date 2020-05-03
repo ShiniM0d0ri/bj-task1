039a6f5c9b054e7a54c6c9755db0f8afc8a8de34
unknown <vnchoudhary357@gmail.com>
2020-05-04 02:57:23 +0530

    Saved Entries in Workbook

--- pytest.py
+++ pytest.py
@@ -1,5 +1,26 @@
 import test_info
+import openpyxl
 
-print("Enter comp name")
-comp_name=input()
-print(test_info.spider(comp_name))
+wb = openpyxl.load_workbook('List of Recognized Startups.xlsx')
+dirmail=[]
+
+print("Enter starting row and ending row :D ")
+rowinit=int(input())
+rowend=int(input())
+
+
+ws=wb.active
+for i in range(rowinit,rowend+1):
+    row=str(i)
+    comp_name=ws['B'+row].value
+    
+    print("Company Name: ",comp_name)
+    dirmail=test_info.spider(comp_name)
+
+    ws['D'+row]=dirmail[0]
+    ws['E'+row]=dirmail[1]
+    print("Entered Successfully......")
+
+wb.save("updated.xlsx")
+print("updated.xlsx saved successfully")
+print(":) :) :)")
